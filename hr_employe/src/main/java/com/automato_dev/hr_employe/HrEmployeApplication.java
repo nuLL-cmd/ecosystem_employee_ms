341e429a8db69a226d58b892e4aed567a8ad0188
root <root@kali.nuLL>
2021-08-01 18:46:24 -0300

    Initial commit

---
+++ hr_employe/src/main/java/com/automato_dev/hr_employe/HrEmployeApplication.java
@@ -0,0 +1,13 @@
+package com.automato_dev.hr_employe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEmployeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEmployeApplication.class, args);
+	}
+
+}
