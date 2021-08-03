ad858ec59cccde4cdb4736296b0f13028d8f063c
root <root@kali.nuLL>
2021-08-02 23:39:24 -0300

    RestTemplate implemented

---
+++ hr_payroll/src/main/java/com/automato_dev/hr_payroll/entity/EmployeeEntity.java
@@ -0,0 +1,28 @@
+﻿package com.automato_dev.hr_payroll.entity;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@JsonInclude(value = Include.NON_NULL)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class EmployeeEntity implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+}
--- hr_payroll/src/main/java/com/automato_dev/hr_payroll/service/PaymentService.java
+++ hr_payroll/src/main/java/com/automato_dev/hr_payroll/service/PaymentService.java
@@ -1,15 +1,32 @@
 ﻿package com.automato_dev.hr_payroll.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.automato_dev.hr_payroll.entity.EmployeeEntity;
 import com.automato_dev.hr_payroll.entity.Payment;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-employee.host}")
+    private String employeeHost;
+
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public Payment getPayment(Long idEmployee, int days){
 
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> params = new HashMap<>();
+        params.put("id", String.valueOf(idEmployee));
+        EmployeeEntity employeeEntity = restTemplate.getForObject(employeeHost+"/employees/{id}", EmployeeEntity.class, params);
+
+        return new Payment(employeeEntity.getName(), employeeEntity.getDailyIncome(), days);
     }
 }
