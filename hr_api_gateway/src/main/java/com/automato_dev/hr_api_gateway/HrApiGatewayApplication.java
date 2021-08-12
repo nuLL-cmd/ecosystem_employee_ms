97bf6bfcd8113f62025a065d366cf8c782752271
hknull <hknull@kali.nuLL>
2021-08-11 22:47:46 -0300

    Configure zuul gateway in workspace

--- hr_api_gateway/src/main/java/com/automato_dev/hr_api_gateway/HrApiGatewayApplication.java
+++ hr_api_gateway/src/main/java/com/automato_dev/hr_api_gateway/HrApiGatewayApplication.java
@@ -2,7 +2,12 @@ package com.automato_dev.hr_api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class HrApiGatewayApplication {
 
